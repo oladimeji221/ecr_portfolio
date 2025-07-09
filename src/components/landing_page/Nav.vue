7f5dd3f1b80ed4167f199c0431e074d741d5cf57
Gideon <giden.oladimeji@ecr-ts.com>
2025-07-09 09:47:40 +0100

    Initial commit

--- src/components/landing_page/Nav.vue
+++ src/components/landing_page/Nav.vue
@@ -5,7 +5,7 @@
         <div class="flex lg:flex-1">
           <a href="#" class="-m-1.5 p-1.5">
             <span class="sr-only">Your Company</span>
-            <img class="h-8 w-auto" src="/src/assets/img/logo.png" alt="" />
+            <img class="h-8 w-auto" src="@/assets/img/logo.png" alt="" />
           </a>
         </div>
         <div class="flex lg:hidden">
@@ -28,7 +28,7 @@
           <div class="flex items-center justify-between">
             <a href="#" class="-m-1.5 p-1.5">
               <span class="sr-only">Your Company</span>
-              <img class="h-8 w-auto" src="../assets/img/logo.png" alt="" />
+              <img class="h-8 w-auto" src="@/assets/img/logo.png" alt="" />
             </a>
             <button type="button" @click="mobileMenuOpen = false" class="-m-2.5 rounded-md p-2.5 text-gray-700">
               <span class="sr-only">Close menu</span>
